c186302cffdbdbb4e5bb88bde46e02da4ee71986
Shailesh <shaileshwakpaijan@gmail.com>
2024-05-24 06:52:59 -0400

    cors bug fixed

--- Backend/app.js
+++ Backend/app.js
@@ -3,14 +3,19 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const userRoute = require("./routes/user.router")
-const postRoute = require("./routes/post.router")
-const followRoute = require("./routes/follow.router")
-const likeRoute = require("./routes/like.router")
-const saveRoute = require("./routes/save.router")
-const commentRoute = require("./routes/comment.router")
+const userRoute = require("./routes/user.router");
+const postRoute = require("./routes/post.router");
+const followRoute = require("./routes/follow.router");
+const likeRoute = require("./routes/like.router");
+const saveRoute = require("./routes/save.router");
+const commentRoute = require("./routes/comment.router");
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
@@ -18,8 +23,8 @@ app.get("/", (req, res) => {
   res.json("Working");
 });
 
-app.use("/api/v1/user", userRoute)
-app.use("/api/v1/post", postRoute)
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
 app.use("/api/v1/follow", followRoute);
 app.use("/api/v1/like", likeRoute);
 app.use("/api/v1/save", saveRoute);
@@ -29,6 +34,5 @@ app.use((err, req, res, next) => {
   let { success, statusCode = 500, message = "Something Went Wrong!" } = err;
   res.status(statusCode).json({ success, statusCode, message });
 });
-
 
 module.exports = { app };
